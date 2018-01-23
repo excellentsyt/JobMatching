8101558708daec06823bc9e15499081844ed5127
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2018-01-23 23:29:25 +1000

    Test workers and test jobs.

--- src/test/java/com/siyang/SwipeJobsAssess/SwipeJobsAssessApplicationTests.java
+++ src/test/java/com/siyang/SwipeJobsAssess/SwipeJobsAssessApplicationTests.java
@@ -1,16 +1,42 @@
 package com.siyang.SwipeJobsAssess;
 
+import com.siyang.SwipeJobsAssess.Match.Job;
+import com.siyang.SwipeJobsAssess.Match.MatchService;
+import com.siyang.SwipeJobsAssess.Match.Worker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SwipeJobsAssessApplicationTests {
 
+	@Autowired
+	private MatchService matchService;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testGetWorkers() {
+		Worker[] workers = matchService.getWorkers();
+		assertEquals(50, workers.length);
+	}
+
+	@Test
+	public void testGetJobs() {
+		Job[] jobs = matchService.getJobs();
+		assertEquals(40, jobs.length);
+	}
+
+	@Test
+	public void testMatch() {
+
+	}
 }

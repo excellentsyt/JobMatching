e39f6f4efbc6a86930a9005622541880f59fc982
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2018-01-24 00:26:03 +1000

    Fix import and comments.

--- src/test/java/com/siyang/SwipeJobsAssess/SwipeJobsAssessApplicationTests.java
+++ src/test/java/com/siyang/SwipeJobsAssess/SwipeJobsAssessApplicationTests.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -68,7 +67,6 @@ public class SwipeJobsAssessApplicationTests {
 
 	/**
 	 *Must meet conditions:
-	 -- isActive
 	 -- driver licence
 	 -- certificates matching
 	 -- distance matching
